2a33346afb5fa7814ed001aa310b0f7b44734de0
luckflyTU <128443786+luckflyTU@users.noreply.github.com>
2025-12-16 17:27:28 +0800

    停車距離修改

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -110,7 +110,8 @@ def get_STOP_DISTANCE(personality=log.LongitudinalPersonality.standard):
   elif personality==log.LongitudinalPersonality.standard:
     return 4.0
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 4.0
+    #return 4.0
+    return 3.0
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
