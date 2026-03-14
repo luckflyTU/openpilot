b9296e94ed38b3ae4b5590c07b0d1d3192510c60
luckflyTU <luckfly@msn.com>
2026-03-14 12:14:35 +0800

    Remove stray block comment markers in hud.cc
    Clean up a commented-out CPU status block in HudRenderer::drawCpuStatus by removing leftover '/*' and '*/' markers that were present inside single-line comments. This is a non-functional change to improve readability and avoid confusing nested comment markers.

--- selfdrive/ui/qt/onroad/hud.cc
+++ selfdrive/ui/qt/onroad/hud.cc
@@ -347,7 +347,7 @@ void HudRenderer::drawCpuStatus(QPainter &p, const QRect &surface_rect) {
     // Default color
     QColor text_color = QColor(255, 255, 255, 255);
 
-    // /*
+    //
     // // Determine the text color based on the card thread status
     // if (!card_thread_status.isEmpty()) {
     //    // Green if Running, Red if Error/Stopped
@@ -360,7 +360,7 @@ void HudRenderer::drawCpuStatus(QPainter &p, const QRect &surface_rect) {
     //     // Blue if status is unknown/empty
     //     text_color = QColor(0, 0, 255, 255);
     // }
-    // * /
+    //
 
     // Format CPU usage strings
     QString core_disp1;
